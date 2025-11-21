67a1a52091496a0a1742832a216f357c536a15bd
HUGO MORIN <oguhom@outlook.fr>
2025-11-21 18:20:49 +0100

    Initial commit

---
+++ src/main/java/com/company/expense/secure_expense/SecureExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.company.expense.secure_expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureExpenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/expense/secure_expense/SecureExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.expense.secure_expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
